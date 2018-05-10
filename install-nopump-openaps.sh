b2a29cbfeac65aa4080a925ec6b02b95ed143863
Charles Eldridge <36391254+xformbykr@users.noreply.github.com>
2018-05-10 16:39:42 -0400

    Update install-nopump-openaps.sh
    shell commands for installation under hypothetical username "jrd", within
    hypothetical directory 'myOpenAPS'.

--- install-nopump-openaps.sh
+++ install-nopump-openaps.sh
@@ -1,1 +1,14 @@
-# commands for installing openAPS when you have no pump
+# commands for installing openAPS and oref0 when you have no pump
+sudo apt-get -o  Acquire::ForceIPv4=true install -y git python python-dev software-properties-common python-numpy python-pip nodejs-legacy npm watchdog vim locate jq lm-sensors
+sudo pip install git+https://github.com/openaps/openaps.git@nogit
+sudo openaps-install-udev-rules
+sudo activate-global-python-argcomplete
+sudo npm install -g json oref0
+sudo pip install -U openaps
+sudo pip install -U openaps-contrib
+sudo activate-global-python-argcomplete
+sudo npm install -g json oref0
+openaps --version
+git config --global user.email "jrd@example.com"
+git config --global user.name "jrd"
+openaps init myOpenAPS
